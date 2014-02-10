0a4f7f7128c673efb2e20379f0fb563bce389a73
Christopher Barry <cjbarry@rogers.com>
2014-02-10 10:34:25 -0500

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <Position.h>
+#include <Inventory.h>
+
+class Player
+{
+    Public:
+        getPosition();
+        getInventory();
+        setPosition(Position);
+    Private:
+        Position playerPosition;
+        Inventory PlayerInventory;
+}
