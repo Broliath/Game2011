7fda8ce71c7834f7729129002206ea2f1a6c46e8
Christopher Barry <cjbarry@rogers.com>
2014-02-10 10:35:21 -0500

    Create Position.cpp

---
+++ Position.cpp
@@ -0,0 +1,25 @@
+//set the position
+void Position::setPostition(int x, int y)
+{
+    X = x;
+    Y = y;
+}
+
+//return the X position
+int Position::getX()
+{
+    return X;
+}
+
+//return the Y position
+int Position::getY()
+{
+    return Y;
+}
+
+//constructor
+Position::Position(int x, int y)
+{
+    X = x;
+    Y = y;
+}
